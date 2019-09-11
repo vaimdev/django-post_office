146649cf4cc40eb032f363c38c7dfbc0eba07130
vaimdev <dev@v-aim.com>
2019-09-12 00:50:48 +0800

    Simplify complex query that cause djongo error
     SQLDecodeError

--- post_office/mail.py
+++ post_office/mail.py
@@ -173,9 +173,8 @@ def get_queued():
      - Has scheduled_time lower than the current time or None
     """
     return Email.objects.filter(status=STATUS.queued) \
-        .select_related('template') \
         .filter(Q(scheduled_time__lte=now()) | Q(scheduled_time=None)) \
-        .order_by(*get_sending_order()).prefetch_related('attachments')[:get_batch_size()]
+        .order_by(*get_sending_order())[:get_batch_size()]
 
 
 def send_queued(processes=1, log_level=None):
